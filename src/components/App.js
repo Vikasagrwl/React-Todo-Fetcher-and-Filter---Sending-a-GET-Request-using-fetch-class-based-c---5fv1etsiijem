98cef3bc0aaade700a92afd2eed9a5180ae06629
Vikasagrwl <105685053+Vikasagrwl@users.noreply.github.com>
2023-03-20 21:16:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/App.css';
 import { Loader } from './Loader';
 import { Todo } from './Todo';
 const App = () => {
+    const[loader, setLoader] = useState(true);
+    const[todo, setTodo] = useState([]);
+    useEffect(()=>{
+        const getData = async ()=>{
+            try {
+                const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+                const data = await response.json();
+                setLoader(false);                
+                setTodo(data.slice(0,20));
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        getData();
+    })
   
-  
+    return(
+        <>
+            {loader? <Loader /> : <Todo todo={todo}/>}
+        </>
+    )
 }
 
 
